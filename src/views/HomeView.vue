9f3ac4d0ad347bb521e1e0006b5cdc1c6b288b3c
kelvin vasquez <kelvin.vasquez@focusservices.com>
2022-11-04 21:05:58 -0600

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home ">
+    <div class="row d-flex justify-content-center">
+      <div class="card col-2">
+        <div class="card-header">
+       Contador
+        </div>
+        <div class="card-body">
+          <h1>{{counter}}</h1>
+        </div>
+      </div>
+    </div>
+    <button v-on:click="increment" class="btn btn-primary m-2"> Incrementar</button>
+    <button v-on:click="decrement" class="btn btn-success">Decrementar</button>
   </div>
+
 </template>
 
 <script>
@@ -13,6 +24,19 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment: function (){
+     this.counter++;
+    },
+    decrement: function(){
+      this.counter--;
+    }
   }
 }
 </script>
